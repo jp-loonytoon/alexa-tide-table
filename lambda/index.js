7744708e21987297e704f6d658ac8ddd715291eb
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-26 23:15:59 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -27,8 +27,6 @@ const GetNextTideHandler = {
         && request.intent.name === 'NextHighTideIntent');
   },
   handle(handlerInput) {
-    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-    
     const speakOutput = 'The tide at Portsmouth is coming in now, and high tide will be in 50 minutes, at 9.45pm'
 
     return handlerInput.responseBuilder
@@ -36,7 +34,6 @@ const GetNextTideHandler = {
       // Uncomment the next line if you want to keep the session open so you can
       // ask for another fact without first re-opening the skill
       // .reprompt(requestAttributes.t('HELP_REPROMPT'))
-      .withSimpleCard(requestAttributes.t('SKILL_NAME'), randomFact)
       .getResponse();
   },
 };
