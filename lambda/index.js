a8559d529bb1e73c1470d0934f3b2885b7b72688
James Page <james@ecreationtech.co.uk>
2020-11-27 22:31:22 +0000

    Added IntentReflectorHandler

--- lambda/index.js
+++ lambda/index.js
@@ -110,6 +110,25 @@ const ErrorHandler = {
 };
 
 
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+  },
+  handle(handlerInput) {
+      const intentName = handlerInput.requestEnvelope.request.intent.name;
+      const speechText = `You just triggered ${intentName}`;
+
+      return handlerInput.responseBuilder
+          .speak(speechText)
+          //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+          .getResponse();
+  }
+};
+
 // log the JSON request to Amazon CloudWatch each time a request is received by the skill. 
 const RequestLog = {
     process(handlerInput) {
@@ -127,8 +146,13 @@ exports.handler = skillBuilder
     HelpHandler,
     ExitHandler,
     SessionEndedRequestHandler,
+    IntentReflectorHandler
   )
-  .addErrorHandlers(ErrorHandler)
-  .addRequestInterceptors(RequestLog)
+  .addErrorHandlers(
+    ErrorHandler
+  )
+  .addRequestInterceptors(
+    RequestLog
+  )
   .withCustomUserAgent('tide-table/v1')
   .lambda();
